767681d687ff9d0aafa93b11199ba4492cda8089
SamhithaRamarathnam <165411013+SamhithaRamarathnam@users.noreply.github.com>
2025-07-16 14:19:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-# DB connection setup will go here
+import os
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT", "5432")  
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
